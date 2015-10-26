37b0f4ecef04e581b0a7ae8555dbf79011f776ac
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-10-26 17:27:45 -0400

    removed duplicates and added template function

--- main.js
+++ main.js
@@ -7,125 +7,39 @@ var nextPic = "";
 
 $(document).ready(function(){
 
+var templating = function(tmpl, $el, data){
+  var template =_.template($(tmpl).html());
+  var addHTML ="";
+
+  _.each(data, function(currVal, idx, arr){
+    addHTML += template(currVal);
+  });
+
+  $el.html(addHTML);
+
+};
 
 //create home page
-var albumTemplate = _.template($('#albumTmpl').html());
-var albumHTML = "";
+templating('#albumTmpl', $('#home'), albums);
 
-_.each(albums, function(currVal, idx, arr){
-  albumHTML += albumTemplate(currVal);
-
-});
-
-$('#home').html(albumHTML);
-
-//Making Nav Menu
-
-var navTemplate = _.template($('#navTmpl').html());
-var navHTML ="";
-
-_.each(albums, function(currVal, idx, arr){
-
-  navHTML += navTemplate(currVal);
-
-});
-
-
-$('aside').html(navHTML);
+// create nav bar
+templating('#navTmpl', $('aside'), albums);
 
 //make single picture
 
 $('#single_picture').html("<section><h1></h1><a href=''> Back to " + selectedAlbum + "</a></section><article id='previous'>Previous</article><article id='current'><img src='" + currPic.url + "'></article><article id='next'>Next</article>");
 
-// Click functionality for photo album on home page bringing you to the single album display
-
-$('.photo-album').on('click', function(event) {
-  event.preventDefault();
-  $('#single_album').addClass('activeSection');
-  $("#start").removeClass('activeSection');
-  selectedAlbum = $(this).attr("rel");
-
-  currPhotos = albums.filter(function(currVal){
-    return currVal.name === selectedAlbum;
-  })[0].photos;
-
-  var photosHTML = "<h1>" + selectedAlbum + "</h1>";
-
-  _.each(currPhotos, function(currVal, idx, arr){
-
-    photosHTML += "<article id='"+ idx + "' class ='pic' rel ='" + selectedAlbum + "'><img src ='" + currVal.url+"'><h3>" + currVal.name+ "</h3></article>";
 
 
-  });
+//create action for clicking on a picture in album view
 
-  $('#photos').html(photosHTML);
-
-// Setting click event to bring you to single picture viewing
-$('.pic').on('click', function(event) {
-  event.preventDefault();
-  selectedPic = parseInt($(this).attr("id"));
-  $("#single_photo").addClass('activeSection');
-  $("#single_album").removeClass('activeSection');
-  // set current, previous, and next pics. conditional on position in array
-  currPic = currPhotos[selectedPic];
-  previousPic = currPhotos[selectedPic -1];
-  nextPic = currPhotos[selectedPic+1];
-  if(selectedPic === 0){
-    previousPic = currPhotos[currPhotos.length -1];
-  }
-  else if (selectedPic === (currPhotos.length -1)){
-    nextPic = currPhotos[0];
-  }
-
-  $('#current').html("<img src='" + currPic.url +"'>");
-  $('#single_picture h1').html(currPic.name);
-  $('#single_picture a').html("Back to " + selectedAlbum);
-  $('#single_picture a').on('click', function(event){
-    event.preventDefault();
-    $("#single_photo").removeClass('activeSection');
-    $("#single_album").addClass('activeSection');
-
-  });
-});
-
-
-//Click to see previous picture in album
-
-
-//click to see next picture in album
-
-
-
-  });
-
-
-//Click functionality for changing album displayed in single album view
-
-$('aside a').on('click', function(event) {
-  event.preventDefault();
-  var clickedSection = "." + $(this).attr('rel');
-  selectedAlbum = $(this).attr("rel");
-
-  currPhotos = albums.filter(function(currVal){
-    return currVal.name === selectedAlbum;
-  })[0].photos;
-
-  var photosHTML = "<h1>" + selectedAlbum + "</h1>";
-
-  _.each(currPhotos, function(currVal, idx, arr){
-
-    photosHTML += "<article id='"+ idx + "' class ='pic' rel ='" + selectedAlbum + "'><img src ='" + currVal.url+"'><h3>" + currVal.name+ "</h3></article>";
-
-  });
-
-  $('#photos').html(photosHTML);
-
+var picClick = function(){
   $('.pic').on('click', function(event) {
     event.preventDefault();
     selectedPic = parseInt($(this).attr("id"));
     $("#single_photo").addClass('activeSection');
     $("#single_album").removeClass('activeSection');
-    // Set current, previous, and next pics
+    // set current, previous, and next pics. conditional on position in array
     currPic = currPhotos[selectedPic];
     previousPic = currPhotos[selectedPic -1];
     nextPic = currPhotos[selectedPic+1];
@@ -145,12 +59,44 @@ $('aside a').on('click', function(event) {
       $("#single_album").addClass('activeSection');
 
     });
+  });
+
+};
+
+// Click functionality for photo album on home page bringing you to the single album display
+
+var loadAlbum = function(selAlbum){
+  event.preventDefault();
+  $('#single_album').addClass('activeSection');
+  $("#start").removeClass('activeSection');
+  //selectedAlbum = $(this).attr("rel");
+
+  currPhotos = albums.filter(function(currVal){
+    return currVal.name === selectedAlbum;
+  })[0].photos;
+
+  var photosHTML = "<h1>" + selectedAlbum + "</h1>";
+
+  _.each(currPhotos, function(currVal, idx, arr){
+
+    photosHTML += "<article id='"+ idx + "' class ='pic' rel ='" + selectedAlbum + "'><img src ='" + currVal.url+"'><h3>" + currVal.name+ "</h3></article>";
 
 
   });
 
-});
+  $('#photos').html(photosHTML);
 
+  // Setting click event to bring you to single picture viewing
+
+  picClick();
+
+};
+
+$('.photo-album, aside a').on('click', function(event) {
+  selectedAlbum = $(this).attr("rel");
+  loadAlbum(selectedAlbum);
+
+  });
 
 
 $('#previous').on('click', function(event){
@@ -194,6 +140,5 @@ $('#next').on('click', function(event){
     nextPic = currPhotos[0];
   }
 });
-
 
 });
